ab946648d89943aea005840af571d2a92133e277
lvnkae <cpdash@yahoo.co.jp>
2018-01-15 03:24:39 +0900

    汎用処理lib化終わり
    garnet-libのincludeパス、libパス、リンクファイル追加
    boostのバージョンを上げた(1.64.0.0 -> 1.66.0.0)
    　NuGetで古いboostを入れる方法がなく、garnet側が1.66.0.0になってしまったため

--- stdafx.h
+++ stdafx.h
@@ -13,8 +13,8 @@
 
 // C ランタイム ヘッダー ファイル
 #include <stdlib.h>
-#include <malloc.h>
-#include <memory.h>
+//#include <malloc.h>
+//#include <memory.h>
 #include <tchar.h>
 
 
@@ -22,24 +22,4 @@
 #pragma warning(disable:4351)
 #endif /* _MSC_VER */
 
-#include <cstdint>
-typedef float float32;
-typedef double float64;
-
-// pythonバージョン(2.7 => 2700)
-#define _PYTHON_VER 2700
-
-#if (2000 <= _PYTHON_VER && _PYTHON_VER < 3000)
-#elif (3000 <= _PYTHON_VER && _PYTHON_VER < 4000)
-#define PYTHON_USE_WCHAR
-#else
-#error
-#endif /* _PYTHON_VER */
-
-#if defined(PYTHON_USE_WCHAR)
-typedef wchar_t PYCHAR;
-#define PYSTRING std::wstring
-#else
-typedef char PYCHAR;
-#define PYSTRING std::string
-#endif/* defined(PYTHON_USE_WCHAR) */
+#include "garnet-lib.h"
